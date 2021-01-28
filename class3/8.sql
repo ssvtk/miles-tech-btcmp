014c5dd6ed4967daa1ff0c6855a89f42ec40bfde
Slava Tkachenko <49266473+ssvtk@users.noreply.github.com>
2021-01-29 02:22:00 +0300

    Update 8.sql

--- class3/8.sql
+++ class3/8.sql
@@ -1,1 +1,1 @@
-SELECT (CASE WHEN [level] = 1) THEN ‘Newbie’ WHEN [level] > 1 AND [level] < 10 THEN ‘Apprentice’ ELSE ‘Jorneyman’ END) as class_title, * [FROM GuestClass]
+SELECT (CASE WHEN [level] = 1) THEN ‘Newbie’ WHEN [level] > 1 AND [level] < 10 THEN ‘Apprentice’ ELSE ‘Jorneyman’ END) as class_title, * [FROM guest_class]
