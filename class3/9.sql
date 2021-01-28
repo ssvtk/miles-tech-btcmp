2239bae8a1a172c0b137cde3d37af0e32e2594fc
Slava Tkachenko <49266473+ssvtk@users.noreply.github.com>
2021-01-29 02:00:37 +0300

    Update 9.sql

--- class3/9.sql
+++ class3/9.sql
@@ -1,5 +1,5 @@
 INSERT INTO tavern (owner, floor_count, name)
 SELECT name, id, notes FROM guest;
 
-SELECT CONCAT(RNDCOL_name, ' ', DATA_TYPE, 'hi', TABLE_SCHEMA), * 
+SELECT CONCAT(RNDCOL_name, ' ', DATA_TYPE, 'hola', TABLE_SCHEMA), * 
 FROM INFORMATION_SCHEMA.COLOMNS WHERE TABLE NAME = 'Taverns';
